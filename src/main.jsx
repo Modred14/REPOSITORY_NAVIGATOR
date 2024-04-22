1ccc993aecf29f46b6d8b0c3e74dc0a5d1d46ee1
Favour Omirin <favourdomirin@gmail.com>
2024-04-22 23:17:31 +0100

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={QueryClient}>
+      <QueryClientProvider client={queryClient}>
         <ChakraProvider>
           <App />
         </ChakraProvider>
