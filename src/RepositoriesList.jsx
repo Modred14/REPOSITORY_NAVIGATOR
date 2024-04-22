7644849dcccb77f5cb8def26dde3289e4d5244f0
Favour Omirin <favourdomirin@gmail.com>
2024-04-22 20:43:19 +0100

    First Commit

--- src/RepositoriesList.jsx
+++ src/RepositoriesList.jsx
@@ -4,10 +4,13 @@ import axios from 'axios';
 import { useState } from 'react';
 import Paginate from './pagination';
 import './repoList.css'
+
 const token = import.meta.env.VITE_ACCESS_TOKEN || process.env.VITE_ACCESS_TOKEN;
-console.log(token)
+console.log(token);
+
 const fetchRepositories = async () => {
   try {
+    const data = await axios.get(api); 
     const response = await axios.get('https://api.github.com/users/Modred14', {
       headers: {
         Authorization: `Bearer ${token}`
@@ -24,45 +27,46 @@ function RepositoriesList () {
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage, setPerPage] = useState(2);
   const [searchQuery, setSearchQuery] = useState('');
-  const { isLoading, isError, data } = useQuery(['repositories'],() => fetchRepositories());
+
+  const { isLoading, isError, data } = useQuery(['repositories'], fetchRepositories);
   const lastIndex = currentPage * perPage;
   const firstIndex = lastIndex - perPage;
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error fetching repositories</div>;
+
   const filteredRepositories = data.filter(repo =>
     repo.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  const currentItems = filteredRepositories.slice(firstIndex, lastIndex)
+  const currentItems = filteredRepositories.slice(firstIndex, lastIndex);
 
   return (
-  <div className="header">
-    <h1>MY PORTFOLIO</h1>
-    <section className='repoList'>
-      <h2>My Repositories</h2>
-      <input
-          type="text"
-          placeholder="Search repositories..."
-          className="search-repo"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-      <ul>
-        {currentItems.map((repo, index) => (
-          <li key={index}>
-            <span>{repo.name.toUpperCase()}</span>
-            <button className='infoBTN'>
-              <Link className='link' to={`/repository/${repo.id}`}>DETAILS</Link>
-            </button>
-          </li>
-        ))}
-      </ul>
-      <Paginate totalItems={filteredRepositories.length} perPage={perPage} setCurrentPage={setCurrentPage} currentPage={currentPage} />
-    </section>
-  </div>  
+    <div className="header">
+      <h1>MY PORTFOLIO</h1>
+      <section className='repoList'>
+        <h2>My Repositories</h2>
+        <input
+            type="text"
+            placeholder="Search repositories..."
+            className="search-repo"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        <ul>
+          {currentItems.map((repo, index) => (
+            <li key={index}>
+              <span>{repo.name.toUpperCase()}</span>
+              <button className='infoBTN'>
+                <Link className='link' to={`/repository/${repo.id}`}>DETAILS</Link>
+              </button>
+            </li>
+          ))}
+        </ul>
+        <Paginate totalItems={filteredRepositories.length} perPage={perPage} setCurrentPage={setCurrentPage} currentPage={currentPage} />
+      </section>
+    </div>  
   );
 }
-
 
 export default RepositoriesList;
