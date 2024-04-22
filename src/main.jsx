05a35473196f3de67fec96d364760c717dfb598f
Favour Omirin <favourdomirin@gmail.com>
2024-04-22 23:44:27 +0100

    Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
